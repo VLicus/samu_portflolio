0bf17fd47089085f59f868246545b2b2ffed9bbd
Samuel Cobas <samu.cobas.ort@gmail.com>
2023-05-18 20:59:56 +0100

    feat: owner name acquired

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
   website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
+  author: "Samuel Cobas",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "Samuel Cobas Portfolio",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
